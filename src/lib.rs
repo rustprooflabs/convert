5fe70db9af9169faaa903b26574c9b44ecc2ae43
Ryan Lambert <ryan@rustprooflabs.com>
2022-11-15 17:45:34 -0700

    Remove return statements

--- src/lib.rs
+++ src/lib.rs
@@ -6,71 +6,71 @@ pgx::pg_module_magic!();
 
 #[pg_extern]
 fn dist_mi_to_ft(miles: f64) -> f64 {
-    return miles * 5280.0;
+    miles * 5280.0
 }
 
 #[pg_extern]
 fn dist_ft_to_mi(feet: f64) -> f64 {
-    return feet / 5280.0;
+    feet / 5280.0
 }
 
 
 #[pg_extern]
 fn dist_ft_to_m(feet: f64) -> f64 {
-    return feet * 0.3048;
+    feet * 0.3048
 }
 
 #[pg_extern]
 fn dist_m_to_km(meters: f64) -> f64 {
-    return meters / 1000.0;
+    meters / 1000.0
 }
 
 #[pg_extern]
 fn dist_km_to_m(kilometers: f64) -> f64 {
     // inverse of m_to_km conversion
-    return 1.0 / dist_m_to_km(kilometers);
+    1.0 / dist_m_to_km(kilometers)
 }
 
 
 #[pg_extern]
 fn dist_mi_to_km(miles: f64) -> f64 {
-    return dist_m_to_km(dist_ft_to_m(dist_mi_to_ft(miles)))
+    dist_m_to_km(dist_ft_to_m(dist_mi_to_ft(miles)))
 }
 
 
 #[pg_extern]
 fn dist_m_to_ft(meters: f64) -> f64 {
-    return meters * 3.28084;
+    meters * 3.28084
 }
 
 #[pg_extern]
 fn dist_km_to_mi(kilometers: f64) -> f64 {
-    return dist_ft_to_mi(dist_m_to_ft(dist_km_to_m(kilometers)));
+    dist_ft_to_mi(dist_m_to_ft(dist_km_to_m(kilometers)))
 }
 
 
 // mph and km/hr
 #[pg_extern]
 fn speed_mph_to_kmhr(miles_per_hour: f64) -> f64 {
-    return miles_per_hour * 1.609344;
+    miles_per_hour * 1.609344
 }
 
 #[pg_extern]
 fn speed_kmhr_to_mph(kilometers_per_hour: f64) -> f64 {
-    return kilometers_per_hour * 0.621;
+    kilometers_per_hour * 0.621
 }
 
 
 #[pg_extern]
 fn speed_kmhr_to_m_s(kmhr: f64) -> f64 {
-    return kmhr * 0.27777777777778;
+    kmhr * 0.27777777777778
 }
 
 
 // speed + distance -> time
 #[pg_extern]
 fn ttt_meters_m_s(length_meters: f64, meters_per_second: f64) -> f64 {
-    return length_meters / meters_per_second;
+    length_meters / meters_per_second
 }
 
 
